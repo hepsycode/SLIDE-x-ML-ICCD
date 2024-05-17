aca1446111c25b13cb2d5b9f1f21beeca3f22b17
Vittoriano Muttillo <vittoriano.muttillo@gmail.com>
2024-05-17 18:06:04 +0200

    commit changes
    commit changes new

---
+++ SLIDE-x-Model-Engineering/SILDE-x-Model-Training/Artix-7/Test.m
@@ -0,0 +1,143 @@
+%% Initial Clean
+%clc
+%clear
+
+%% Parameters Area
+load("Area\trainedModel.mat")
+filepath = 'Artix-7_Area.csv';
+T = readtable(filepath,'VariableNamingRule','preserve');
+
+n = height(T);
+m = width(T);
+% kkk = (n * 20)./100
+c = cvpartition(n,'HoldOut',0.2);  % 'HoldOut',0.2);
+idxTrain = training(c,1);
+idxTest = ~idxTrain;
+X_train = T(idxTrain,:);
+y_train = T(idxTrain,m);
+X_test = T(idxTest,1:m-1);
+y_test = T(idxTest,m);
+
+yReal = y_test{:,1}; % real values
+
+%% Variables
+timeTrain = [0 0 0 0 0 0 0 0 0];
+timePrediction = [0 0 0 0 0 0 0 0 0];
+validationRMSET = [0 0 0 0 0 0 0 0 0];
+
+%% Parameters
+i = 1;
+%size_X_test = size(X_test);
+
+%% Linear Regression
+%tic
+% [LinearModel, validationRMSET(i)] = trainRegressionModel_00_LR(X_train);
+% timeTrain(i) = toc;
+
+tic
+yfit = trainedModel_00_LR.predictFcn(X_test);  % predicted values
+timePrediction(i) = toc; % /size_X_test(1);
+
+[EV_00_LR, EVP_00_LR, NRV_00_LR] = errCalcFunc(yfit,yReal);
+i = i+1;
+
+%% Fine Tree
+%tic
+% [FineTreeModel, validationRMSET(i)] = trainRegressionModel_01_FT(X_train);
+% timeTrain(i) = toc;
+
+tic
+yfit = trainedModel_01_FT.predictFcn(X_test);  % predicted values
+timePrediction(i) = toc;
+
+[EV_01_FT, EVP_01_FT, NRV_01_FT] = errCalcFunc(yfit,yReal);
+i = i+1;
+
+%% Medium Tree
+%tic
+% [MediumTreeModel, validationRMSET(i)] = trainRegressionModel_02_MT(X_train);
+% timeTrain(i) = toc;
+
+tic
+yfit = trainedModel_02_MT.predictFcn(X_test);  % predicted values
+timePrediction(i) = toc;
+
+[EV_02_MT, EVP_02_MT, NRV_02_MT] = errCalcFunc(yfit,yReal);
+i = i+1;
+
+%% Coarse Tree
+%tic
+% [CoarseTreeModel, validationRMSET(i)] = trainRegressionModel_03_CT(X_train);
+% timeTrain(i) = toc;
+
+tic
+yfit = trainedModel_03_CT.predictFcn(X_test);  % predicted values
+timePrediction(i) = toc;
+
+[EV_03_CT, EVP_03_CT, NRV_03_CT] = errCalcFunc(yfit,yReal);
+i = i+1;
+
+%% SVM Linear
+%tic
+% [LinearSVMModel, validationRMSET(i)] = trainRegressionModel_04_SVM_L(X_train);
+% timeTrain(i) = toc;
+
+tic
+yfit = trainedModel_04_SVM_L.predictFcn(X_test);  % predicted values
+timePrediction(i) = toc;
+
+[EV_04_SVM_L, EVP_04_SVM_L, NRV_04_SVM_L] = errCalcFunc(yfit,yReal);
+i = i+1;
+
+%% SVM Quadratic
+%tic
+% [QuadraticSVMModel, validationRMSET(i)] = trainRegressionModel_05_SVM_Q(X_train);
+% timeTrain(i) = toc;
+
+tic
+yfit = trainedModel_05_SVM_Q.predictFcn(X_test);  % predicted values
+timePrediction(i) = toc;
+
+[EV_05_SVM_Q, EVP_05_SVM_Q, NRV_05_SVM_Q] = errCalcFunc(yfit,yReal);
+i = i+1;
+
+%% SVM Cubic
+%tic
+% [CubicSVMModel, validationRMSET(i)] = trainRegressionModel_06_SVM_C(X_train);
+% timeTrain(i) = toc;
+
+tic
+yfit = trainedModel_06_SVM_C.predictFcn(X_test);  % predicted values
+timePrediction(i) = toc;
+
+[EV_06_SVM_C, EVP_06_SVM_C, NRV_06_SVM_C] = errCalcFunc(yfit,yReal);
+i = i+1;
+
+%% BOT
+%tic
+% [BOTModel, validationRMSET(i)] = trainRegressionModel_07_BOT(X_train);
+% timeTrain(i) = toc;
+
+tic
+yfit = trainedModel_07_BOT.predictFcn(X_test);  % predicted values
+timePrediction(i) = toc;
+
+[EV_07_BOT, EVP_07_BOT, NRV_07_BOT] = errCalcFunc(yfit,yReal);
+i = i+1;
+
+%% BAT
+%tic
+% [BOTModel, validationRMSET(i)] = trainRegressionModel_08_BAT(X_train);
+% timeTrain(i) = toc;
+
+tic
+yfit = trainedModel_08_BAT.predictFcn(X_test);  % predicted values
+timePrediction(i) = toc;
+
+[EV_08_BAT, EVP_08_BAT, NRV_08_BAT] = errCalcFunc(yfit,yReal);
+i = i+1;
+
+
+
+
+
---
+++ SLIDE-x-Model-Engineering/SILDE-x-Model-Training/Artix-7/errCalcFunc.m
@@ -0,0 +1,29 @@
+function [EV, EVP, NRV] = errCalcFunc(yfit2,yReal2)
+
+    % isNotMissing = ~isnan(yfit) & ~isnan(yReal);
+    
+    yfit = (yfit2(:,1));
+    yReal = (yReal2(:,1));
+
+    MAE = nansum(abs(( yfit - yReal ))) / numel(yReal);
+    MSE = nansum(( yfit - yReal ).^2) / numel(yReal );
+    RMSE = sqrt(nansum(( yfit - yReal ).^2) / numel(yReal ));
+
+    % n = (yReal~=0):  % find indexes
+    % n = numel(yReal);
+
+    MPE = nansum( (yfit - yReal) ./ ((yReal+(yReal==0)*eps)) )  / numel(yReal) .* 100;
+    MAPE = nansum( abs( (yfit - yReal) ) ./ ((yReal+(yReal==0)*eps)) ) / numel(yReal) .* 100;
+    MSPE = nansum( ( (yfit - yReal) ./ ((yReal+(yReal==0)*eps)) ).^2) / numel(yReal);
+    RMSPE = sqrt(MSPE) .* 100;
+
+    NRMSEAve = RMSE /( nanmean(yReal) );
+    NRMSEMaxMin = RMSE /( max(yReal) - min(yReal) );
+    NRMSESd = RMSE /( std(yReal) );
+    NRMSEIQ = RMSE /( quantile(yReal,0.75) - quantile(yReal,0.25) );
+    
+    EV = [RMSE, MAE, MSE];
+    EVP = [MAPE, RMSPE, MSPE, MPE];
+    NRV = [NRMSEAve, NRMSEMaxMin, NRMSESd, NRMSEIQ];
+    
+end
